4bfa2975a6495c84c72bb645fa640f481110f76d
Gwan-Woo-Jeong <gwanuuoo@gmail.com>
2023-01-24 21:03:38 +0900

    Add form auto-install

--- resources/form.js
+++ resources/form.js
@@ -1,3 +1,28 @@
-export const form = async (buildDir, dom, reviewType, serviceKey, shopNo) => {
-  console.log("form");
+import fs from "fs";
+import {
+  addComments,
+  createElementFromHTML,
+  insertAfter,
+  unwrapHTML,
+} from "../utils/dom.js";
+
+const reviewName = "리뷰작성 폼";
+
+export const form = (buildDir, dom, installCode) => {
+  const document = dom.window.document;
+  const newElement = createElementFromHTML(installCode.custom, dom);
+
+  const referenceNode = document.querySelector("[module='board_title_4']");
+
+  if (referenceNode === null) {
+    return console.error(`[${reviewName}] 자동 설치를 진행할 수 없습니다.`);
+  }
+
+  insertAfter(referenceNode, newElement);
+
+  addComments(reviewName, newElement, dom);
+
+  fs.writeFile(buildDir, unwrapHTML(dom.serialize()), (err) => {
+    if (err) throw err;
+  });
 };
