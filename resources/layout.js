d95028cf1db2294dab204d764612be9adafacfaf
Gwan-Woo-Jeong <gwanuuoo@gmail.com>
2023-01-31 15:23:22 +0900

    Add layouts auto-install

--- resources/layout.js
+++ resources/layout.js
@@ -1,3 +1,36 @@
-export const layout = async (buildDir, dom, reviewType, serviceKey, shopNo) => {
-  console.log("layout");
+import fs from "fs";
+import {
+  addComments,
+  createElementFromHTML,
+  DOMtoString,
+  unwrapHTML,
+} from "../utils/dom.js";
+
+const reviewName = "상품 목록 별점";
+
+export const layout = async (buildDir, dom, installCode) => {
+  const document = dom.window.document;
+
+  const script = createElementFromHTML(installCode.script, dom);
+
+  const body = document.querySelector("body");
+
+  if (body) {
+    body.appendChild(script);
+    addComments(reviewName, script, dom, false, true);
+    fs.writeFile(buildDir, unwrapHTML(dom.serialize()), (err) => {
+      if (err) throw err;
+    });
+  } else {
+    fs.writeFile(
+      buildDir,
+      unwrapHTML(
+        dom.serialize() +
+          addComments(reviewName, DOMtoString(script), dom, false, true)
+      ),
+      (err) => {
+        if (err) throw err;
+      }
+    );
+  }
 };
