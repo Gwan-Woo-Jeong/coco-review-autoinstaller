afad19a8eab23d3ec03ea98d829c922e255ba4a0
Gwan-Woo-Jeong <gwanuuoo@gmail.com>
2023-01-31 15:48:47 +0900

    Add review pop up auto-install

--- resources/popUp.js
+++ resources/popUp.js
@@ -1,3 +1,36 @@
-export const popUp = async (buildDir, dom, reviewType, serviceKey, shopNo) => {
-  console.log("popUp");
+import fs from "fs";
+import {
+  addComments,
+  createElementFromHTML,
+  DOMtoString,
+  unwrapHTML,
+} from "../utils/dom.js";
+
+const reviewName = "리뷰 작성 팝업";
+
+export const popUp = async (buildDir, dom, installCode) => {
+  const document = dom.window.document;
+
+  const script = createElementFromHTML(installCode.script, dom);
+
+  const body = document.querySelector("body");
+
+  if (body) {
+    body.appendChild(script);
+    addComments(reviewName, script, dom, false, true);
+    fs.writeFile(buildDir, unwrapHTML(dom.serialize()), (err) => {
+      if (err) throw err;
+    });
+  } else {
+    fs.writeFile(
+      buildDir,
+      unwrapHTML(
+        dom.serialize() +
+          addComments(reviewName, DOMtoString(script), dom, false, true)
+      ),
+      (err) => {
+        if (err) throw err;
+      }
+    );
+  }
 };
