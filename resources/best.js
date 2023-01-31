e3d963a19735480bf4381eceb3ae9f007d0607f1
Gwan-Woo-Jeong <gwanuuoo@gmail.com>
2023-01-31 14:34:40 +0900

    Add best reviews auto-install

--- resources/best.js
+++ resources/best.js
@@ -1,3 +1,27 @@
-export const best = async (buildDir, dom, reviewType, serviceKey, shopNo) => {
-  console.log("best");
+import fs from "fs";
+import {
+  addComments,
+  createElementFromHTML,
+  insertAfter,
+} from "../utils/dom.js";
+
+const reviewName = "베스트 리뷰";
+
+export const best = async (buildDir, dom, installCode) => {
+  const document = dom.window.document;
+  const newElement = createElementFromHTML(installCode.custom, dom);
+
+  const referenceNode = document.querySelector("[module='board_title_4']");
+
+  if (referenceNode === null) {
+    return console.error(`[${reviewName}] 자동 설치를 진행할 수 없습니다.`);
+  }
+
+  insertAfter(referenceNode, newElement);
+
+  addComments(reviewName, newElement, dom);
+
+  fs.writeFile(buildDir, dom.serialize(), (err) => {
+    if (err) throw err;
+  });
 };
