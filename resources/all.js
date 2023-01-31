3000d4ede5ed1b809d28213ef1108620f85e69fd
Gwan-Woo-Jeong <gwanuuoo@gmail.com>
2023-01-31 14:34:19 +0900

    Add all reviews auto-install

--- resources/all.js
+++ resources/all.js
@@ -1,3 +1,27 @@
-export const all = async (buildDir, dom, reviewType, serviceKey, shopNo) => {
-  console.log("all");
+import fs from "fs";
+import {
+  addComments,
+  createElementFromHTML,
+  insertAfter,
+} from "../utils/dom.js";
+
+const reviewName = "전체 리뷰";
+
+export const all = async (buildDir, dom, installCode) => {
+  const document = dom.window.document;
+  const newElement = createElementFromHTML(installCode.custom, dom);
+
+  const referenceNode = document.querySelector("[module='board_title_4']");
+
+  if (referenceNode === null) {
+    return console.error(`[${reviewName}] 자동 설치를 진행할 수 없습니다.`);
+  }
+
+  insertAfter(referenceNode, newElement);
+
+  addComments(reviewName, newElement, dom);
+
+  fs.writeFile(buildDir, dom.serialize(), (err) => {
+    if (err) throw err;
+  });
 };
